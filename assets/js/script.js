07d743724479022a17a660698791b764d0ca0c17
Jonathan Fulgencio <fulgenciojonat@gmail.com>
2018-10-08 21:27:51 +0800

    updated game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 
 	$("#begin").click(function() {
     	$("#main").hide();
+    	
 
   		
 
@@ -18,66 +19,68 @@ const renderQuestions = (stage) => {
 
 	let questions = [
 	  { q: 'Guess the First song',
-	  	choices: ["song2", "song1", "song3"],
-	    a: "song2"
+	  	song: ['<audio controls><source src="./assets/song/song1.mp3" type="audio/mpeg"></audio>'],
+	    a: "oops i did it again"
 	  },
 	  { q: 'Guess the Second song',
-	  	choices: ["song1", "song2", "song3"],
-	    a: "song3"
+	  	song: ['<audio controls><source src="./assets/song/song2.mp3" type="audio/mpeg"></audio>'],
+	    a: "how far i go"
 	  },
 	  { q: 'Guess the Third song',
-	  	choices: ["song3", "song1", "song2"],
-	    a: "song1"
+	  	song: ['<audio controls><source src="./assets/song/song3.mp3" type="audio/mpeg"></audio>'],
+	    a: "a million dreams"
+	  },
+	  { q: 'Guess the Fourth song',
+	  	song: ['<audio controls><source src="./assets/song/song4.mp3" type="audio/mpeg"></audio>'],
+	    a: "wonderwall"
+	  },
+	  { q: 'Game Over!',
+	  	song: ['You win'],
+	    a: "Game by Jonathan | 2018"
 	  }
 	];
 
-	// let choices = [
-	// 	{ 
-	// 		a: 'song2',
-	// 	  	b: 'song1',
-	// 	  	c: 'song3'
-	// 	},
-	// 	{ 
-	// 		a: 'song1',
-	// 	  	b: 'song2',
-	// 	  	c: 'song3'
-	// 	},
-	// 	{ 
-	// 		a: 'song1',
-	// 	  	b: 'song2',
-	// 	  	c: 'song3'
-	// 	}
 
-	// ];
-	let currentquestion = 0;
-	let correctAnswers = 0;
-	// let response ="";
 
 	
 	
 	for (i = 0; i < stage; i++) {
-		// $("#questions").append(questions[i]['q'] + "<br>");
-		// let response = $("input[name=choices]:checked").val();
-		// let x = questions[i]['choices'][0];
-		// let y = questions[i]['choices'][1];
-		// let z = questions[i]['choices'][2];
-		// let input1 = questions[i]['choices'][i]
+
+		if (i < 4){
+	
   		let formHtml =
 	   
 	      '<form id="formko">' +
 	      '<hr class="row">' +
 	      '<h3 class="question">' + questions[i]['q'] + "<br>"+
-	      '<input type="radio" name="choices" value="'+ questions[i]["choices"][0]+'">'+ questions[i]["choices"][0]+
-	      '<input type="radio" name="choices" value="'+ questions[i]["choices"][1]+'">' + questions[i]["choices"][1]+
-	      '<input type="radio" name="choices" value="'+ questions[i]["choices"][2]+'">' + questions[i]["choices"][2]+
+	      '<h3 class="question">' + questions[i]['song'] + "<br>"+
+	      '<label>Answer </label><input type="textbox" name="choices" value="" id ="answer">' +
 	      '<hr class="row">' +
 	      '<span id="result"></span>' +
 	      '<span class="nav">' +
 	      '<button type="button" id="submit" value="submit">submit</button>' +
-	      '<button type="#"  id="next">next</button>' +
+	      '<button type="#"  id="next">exit</button>' +
 	      '</span></form>';
 	
-	       $("#questions").append(formHtml);
+	      $("#questions").html(formHtml);
+	   }else{
+	   	let formHtml =
+	   
+	      '<form id="formko">' +
+	      '<hr class="row">' +
+	      '<h3 class="question">' + questions[i]['q'] + "<br>"+
+	      '<h3 class="question">' + questions[i]['song'] + "<br>"+
+	      '<h4 class="question">' + questions[i]['a'] + "<br>"+
+	      '<hr class="row">' +
+	      '<span id="result"></span>' +
+	      '<span class="nav">' +
+	      '<button type="#"  id="next">exit</button>' +
+	      '</span></form>';
+	
+	       $("#questions").html(formHtml);
+	   }
+
+
 
 	     
 	     
@@ -85,7 +88,10 @@ const renderQuestions = (stage) => {
 
 	$("#submit").on('click', function() {
    
-	 let response = $("input[name=choices]:checked").val();
+	 // let response = $("input[name=choices]:checked").val();
+	 // let response =  $("input:textbox").val();
+	 let response =  document.getElementById("answer").value.toLowerCase();
+
     	if (response == questions[i-1]['a']){	   
 
 	  alert("correct");
@@ -100,6 +106,7 @@ const renderQuestions = (stage) => {
 
 
 	};
+
     });
 
 
